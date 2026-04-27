b538da99f92fe5b4b9ae9a40de291795f282751d
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-27 21:09:54 +0530

    frontend deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://ecom-project1-backend-1.onrender.com',
+    withCredentials: true,
 });
 
 export default api;
